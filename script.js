75295683e45edccbba11e2e001eeb2d3d36c19d9
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-16 11:49:59 +0200

    Add a dialog and styling

--- script.js
+++ script.js
@@ -17,7 +17,32 @@ const myLibrary = [
         pages: 500,
         read: false
     },
+    {
+        title: "Swann's Way",
+        author: "Marcel Proust",
+        pages: 500,
+        read: false
+    },
+    {
+        title: "Swann's Way",
+        author: "Marcel Proust",
+        pages: 500,
+        read: false
+    },
+    {
+        title: "Swann's Way",
+        author: "Marcel Proust",
+        pages: 500,
+        read: false
+    },
 ];
+
+const newBookBtn = document.querySelector(".new-book-btn");
+const dialog = document.querySelector("dialog");
+
+newBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -26,22 +51,20 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function createElement(tag, text) {
+    const element = document.createElement(tag);
+    element.innerText = text;
+    return element;
+};
+
 function addBookToLibrary(book) {
     const grid = document.querySelector(".books-grid");
     const card = document.createElement("div");
     card.classList.add("card");
-
-    const createElement = (tag, text) => {
-        const element = document.createElement(tag);
-        element.innerText = text;
-        return element;
-    };
-
     card.appendChild(createElement("h2", book.title));
     card.appendChild(createElement("p", book.author));
     card.appendChild(createElement("p", `${book.pages} pages`));
     card.appendChild(createElement("p", book.read ? "Read" : "Not read yet"));
-
     grid.appendChild(card);
 }
 
